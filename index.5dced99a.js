d4acda9743f8b5d756fbecbc80ff010754d8a058
Ruslan3571 <114521177+Ruslan3571@users.noreply.github.com>
2023-01-11 17:01:17 +0000

    Deploying to gh-pages from @ Ruslan3571/team-project_cocktails@8026fca601a1c1e1a31f963041fce179df470196 🚀

--- index.5dced99a.js
+++
@@ -1,2 +0,0 @@
-const e={btn:document.querySelector(".nav-btn"),nav:document.querySelector(".nav-wrapper")};e.btn.addEventListener("click",(function({target:t}){e.nav.classList.add("is-open"),document.body.style.overflow="hidden",t.firstElementChild.style.display="none",t.lastElementChild.style.display=""}));
-//# sourceMappingURL=index.5dced99a.js.map

e70b537072ff87a603b7fe0d5ef4d8e976f1bb6e
Ruslan Formanchuk <ruslan12032@gmail.com>
2023-01-11 21:26:52 +0200

    add js changeTheme

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -25,17 +25,19 @@ export function enableScroll() {
   document.body.style.overflow = '';
 }
 
-// (() => {
-//   const menuBtnRef = document.querySelector('[data-menu-button]');
-//   const mobileMenuRef = document.querySelector('[data-menu]');
+// const mobileMenu = () => {
+//   const refs = {
+//     openMenuBtn: document.querySelector('[data-menu-open]'),
+//     closeMenuBtn: document.querySelector('[data-menu-close]'),
+//     menu: document.querySelector('[data-menu]'),
+//   };
 
-//   menuBtnRef.addEventListener('click', () => {
-//     const expanded =
-//       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+//   refs.openMenuBtn.addEventListener('click', toggleModal);
+//   refs.closeMenuBtn.addEventListener('click', toggleModal);
+  
+//   function toggleModal() {
+//     refs.menu.classList.toggle('is-open');
+//   }
+// };
 
-//     menuBtnRef.classList.toggle('is-open');
-//     menuBtnRef.setAttribute('aria-expanded', !expanded);
-
-//     mobileMenuRef.classList.toggle('is-open');
-//   });
-// })();
+// mobileMenu();

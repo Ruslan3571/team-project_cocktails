864a2afd6c8d716d7fe15270c4bb93c7fb5b2561
Yuliia Koriavets <112959119+YuliiaKoriavets@users.noreply.github.com>
2023-01-11 19:13:13 +0200

    added export / import hero-search.js in index.js

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { alphabet } from "./js/hero-search";
+import { numbers } from "./js/hero-search";
+import { markupLetter } from "./js/hero-search";
+import { markupNumbers } from "./js/hero-search";
--- src/js/hero-search.js
+++ src/js/hero-search.js
@@ -1,11 +1,11 @@
 const searchListLetterEl = document.querySelector(".hero__search-list-letters")
 const searchListNumberEl = document.querySelector(".hero__search-list-numbers")
 
-const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+export const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
+export const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
-const markupLetter = alphabet.map((element) => `<li class="hero__search-item">${element}</li>`).join("")
+export const markupLetter = alphabet.map((element) => `<li class="hero__search-item">${element}</li>`).join("")
 searchListLetterEl.innerHTML = markupLetter
 
-const markupNumbers = numbers.map((element) => `<li class="hero__search-item">${element}</li>`).join("")
+export const markupNumbers = numbers.map((element) => `<li class="hero__search-item">${element}</li>`).join("")
 searchListNumberEl.innerHTML = markupNumbers

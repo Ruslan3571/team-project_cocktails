2a51443c179bc35b1fc4978c2d6e445c3ca273bc
Ruslan Formanchuk <ruslan12032@gmail.com>
2023-01-11 19:19:45 +0200

    +

--- src/index.js
+++ src/index.js
@@ -10,3 +10,7 @@ import {
 } from './js/ApiServise';
 import heroSearch from './js/hero-search';
 import mobileMenu from './js/mobileMenu';
+import { alphabet } from './js/hero-search';
+import { numbers } from './js/hero-search';
+import { markupLetter } from './js/hero-search';
+import { markupNumbers } from './js/hero-search';
--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -1,31 +1,41 @@
-const ref ={
-btn:document.querySelector('.nav-btn'),
-nav:document.querySelector('.nav-wrapper'),
-
+const ref = {
+  btn: document.querySelector('.nav-btn'),
+  nav: document.querySelector('.nav-wrapper'),
 };
 
-
-export function openMobileMenu ({target}){
-ref.nav.classList.add('is-open');
-disableScroll();
-target.firstElementChild.style.display ='none';
-target.lastElementChild.style.display ='';
+export function openMobileMenu({ target }) {
+  ref.nav.classList.add('is-open');
+  disableScroll();
+  target.firstElementChild.style.display = 'none';
+  target.lastElementChild.style.display = '';
 }
 
-export function closeMobileMenu({target}){
-    ref.nav.classList.remove('is-open');
-    target.firstElementChild.style.display ='';
-    target.lastElementChild.style.display ='none';
+export function closeMobileMenu({ target }) {
+  ref.nav.classList.remove('is-open');
+  target.firstElementChild.style.display = '';
+  target.lastElementChild.style.display = 'none';
 }
 
-ref.btn.addEventListener('click',openMobileMenu)
+ref.btn.addEventListener('click', openMobileMenu);
 // ref.btn.addEventListener('click',closeMobileMenu)
 export function disableScroll() {
-    document.body.style.overflow = 'hidden';
-  
-  }
-  export function enableScroll() {
-    document.body.style.overflow = '';
-   
-  }
-  
+  document.body.style.overflow = 'hidden';
+}
+export function enableScroll() {
+  document.body.style.overflow = '';
+}
+
+// (() => {
+//   const menuBtnRef = document.querySelector('[data-menu-button]');
+//   const mobileMenuRef = document.querySelector('[data-menu]');
+
+//   menuBtnRef.addEventListener('click', () => {
+//     const expanded =
+//       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+//     menuBtnRef.classList.toggle('is-open');
+//     menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+//     mobileMenuRef.classList.toggle('is-open');
+//   });
+// })();

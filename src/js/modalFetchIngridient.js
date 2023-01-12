fe4d003b4029caf86c90b6914545cf0b190f3816
Yuruy Vytyaganets <yvytyawork@gmail.com>
2023-01-12 20:47:11 +0200

    initial commit

--- src/js/modalFetchIngridient.js
+++ src/js/modalFetchIngridient.js
@@ -8,23 +8,21 @@ console.log(searchIngredientsById(134));
 console.log(searchCocktailById(11007));
 console.log(searchIngredientsByName('Campari'));
 
-// function getIngridientInfo(ingridients) {
-//   const ingridientsInfo = [];
-//   ingridients.forEach(ingridient => {
-//     for (const key in ingridient) {
-//       if (key.includes('strIngredient') && ingridient[key]) {
-//         ingridientsInfo.push(ingridient[key]);
-//       }
-//     }
-//   });
-//   return ingridientsInfo;
-// }
-
 const createIngridientModalMarkup = function (ingridients) {
+  //   console.log('🚀 ~ ingridients', ingridients);
+  //   function getIngridientInfo(ingridients) {
+  //     for (const ingredient of ingridients) {
+  //       if (ingredient[key] === null) {
+  //         return '';
+  //       }
+  //     }
+  //     return ingridients;
+  //   }
   //   console.log(ingridients);
   //   const ingridientsInfo = getIngridientInfo(ingridients);
   //   console.log('🚀 ~ ingridients', ingridientsInfo);
   const markup = ingridients
+
     .map(({ strAlcohol, strIngredient, strType, strDescription, strABV }) => {
       return `<h2 class="modal__header">${strIngredient}</h2>
             <h3 class="alcohol-type">${strType}</h3>

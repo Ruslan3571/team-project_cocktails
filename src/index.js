f7d5ecc3850efa3fb2f633a7c1ac9b2d5669ef8a
Ruslan Formanchuk <ruslan12032@gmail.com>
2023-01-12 20:37:39 +0200

    add js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ import {
   searchIngredientsById,
 } from './js/ApiServise';
 import heroSearch from './js/hero-search';
-import heroSearchMobile from './js/hero-search-mobile'
-import heroDropdown from './js/hero-dropdown'
+import heroSearchMobile from './js/hero-search-mobile';
+import heroDropdown from './js/hero-dropdown';
 import mobileMenu from './js/mobileMenu';
 import changeTheme from './js/changeTheme';
+import renderCocktails from './js/renderCocktails';

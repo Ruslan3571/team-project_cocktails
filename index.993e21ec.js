4c537c65e7ee2ef7f9d5f50098db7628668f36e7
Ruslan3571 <114521177+Ruslan3571@users.noreply.github.com>
2023-01-12 19:48:20 +0000

    Deploying to gh-pages from @ Ruslan3571/team-project_cocktails@37e0c0eb6445c2e0d0a60948544229fd2c6e2e34 🚀

--- index.993e21ec.js
+++
@@ -1,2 +0,0 @@
-!function(){var e;(e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")}).closeModalBtn.addEventListener("click",(function(){e.modal.classList.toggle("is-hidden")}))}();
-//# sourceMappingURL=index.993e21ec.js.map
--- index.a46816b6.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};e.closeModalBtn.addEventListener("click",(function(){e.modal.classList.toggle("is-hidden")}))})();
-//# sourceMappingURL=index.a46816b6.js.map

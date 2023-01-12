b837ada46d6270e8a82619c00b404bf18ad5405d
Ruslan3571 <114521177+Ruslan3571@users.noreply.github.com>
2023-01-12 18:35:15 +0000

    Deploying to gh-pages from @ Ruslan3571/team-project_cocktails@006ec3bfd1a31270fa0df3ba493729ea46e6ba6a 🚀

---
+++ index.a46816b6.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};e.closeModalBtn.addEventListener("click",(function(){e.modal.classList.toggle("is-hidden")}))})();
+//# sourceMappingURL=index.a46816b6.js.map

2d186c8829c426505a431659fd06cd8d50c2f63a
Ruslan Formanchuk <ruslan12032@gmail.com>
2023-01-13 16:48:32 +0200

    Update mobileMenu.js

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -1,28 +1,24 @@
-
-
 const ref = {
   btn: document.querySelector('.nav-btn'),
   nav: document.querySelector('.nav-wrapper'),
   header: document.querySelector('.header'),
-
 };
 
 export function openMobileMenu({ target }) {
-  if(target.nodeName!='svg') return;
-    ref.nav.classList.add('is-open');
-    ref.header.style.overflow = '';
-    disableScroll();
-    target.firstElementChild.style.display = 'none';
-    target.lastElementChild.style.display = 'block';
-  
+  if (target.nodeName != 'svg') return;
+  ref.nav.classList.add('is-open');
+  ref.header.style.overflow = '';
+  disableScroll();
+  target.firstElementChild.style.display = 'none';
+  target.lastElementChild.style.display = 'block';
 }
 
 export function closeMobileMenu({ target }) {
-  if(target.nodeName!='svg') return;
-    ref.nav.classList.remove('is-open');
-    target.firstElementChild.style.display = 'block';
-    target.lastElementChild.style.display = 'none';
-    ref.header.style.overflow = 'hidden';
+  if (target.nodeName != 'svg') return;
+  ref.nav.classList.remove('is-open');
+  target.firstElementChild.style.display = 'block';
+  target.lastElementChild.style.display = 'none';
+  ref.header.style.overflow = 'hidden';
 }
 
 export function disableScroll() {
@@ -36,5 +32,4 @@ ref.btn.addEventListener('click', ({ target }) =>
   ref.nav.classList.contains('is-open')
     ? closeMobileMenu({ target })
     : openMobileMenu({ target })
-    
 );
